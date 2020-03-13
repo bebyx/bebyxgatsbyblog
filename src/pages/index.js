1193cfe9c3d1ea1fd244a85a2e62139b5453ab34
bebyx <artem.bebyk@gmail.com>
2020-03-13 13:26:41 -0500

    Add slugs and template for blog post

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,6 @@ import { Link } from "gatsby"
 import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = ( {data} ) => (
