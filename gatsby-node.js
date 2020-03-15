9fd68b8a65fa7c4757acb93c2f1e27258861561e
bebyx <artem.bebyk@gmail.com>
2020-03-15 22:41:29 +0200

    Slight syntax fix in gatsby-node

--- gatsby-node.js
+++ gatsby-node.js
@@ -36,7 +36,7 @@ exports.createPages = async ({ graphql, actions }) => {
     }
   `)
 
-  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+result.data.allMarkdownRemark.edges.forEach(({ node }) => {
     createPage({
       path: node.fields.slug,
       component: path.resolve(`./src/templates/blog-post.js`),
@@ -46,5 +46,4 @@ exports.createPages = async ({ graphql, actions }) => {
         slug: node.fields.slug,
       },
     })
-  })
-}
+})
