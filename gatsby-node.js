4b1e94fd7c343627346d451beed0829c1192feb3
bebyx <artem.bebyk@gmail.com>
2020-03-15 23:57:13 +0200

    Fix syntax in gatsby-node, clean-up

--- gatsby-node.js
+++ gatsby-node.js
@@ -36,14 +36,15 @@ exports.createPages = async ({ graphql, actions }) => {
     }
   `)
 
-result.data.allMarkdownRemark.edges.forEach(({ node }) => {
-    createPage({
-      path: node.fields.slug,
-      component: path.resolve(`./src/templates/blog-post.js`),
-      context: {
-        // Data passed to context is available
-        // in page queries as GraphQL variables.
-        slug: node.fields.slug,
-      },
-    })
-})
+  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+      createPage({
+        path: node.fields.slug,
+        component: path.resolve(`./src/templates/blog-post.js`),
+        context: {
+          // Data passed to context is available
+          // in page queries as GraphQL variables.
+          slug: node.fields.slug,
+        },
+      })
+  })
+}
