bd1605c4c8d79907aa2307564226688b41cda5df
bebyx <artem.bebyk@gmail.com>
2020-03-18 01:33:05 +0200

    Update meta-title for index page

--- src/components/seo.js
+++ src/components/seo.js
@@ -33,7 +33,7 @@ function SEO({ description, lang, meta, title }) {
         lang,
       }}
       title={title}
-      titleTemplate={`%s | ${site.siteMetadata.title} блоґ`}
+      titleTemplate={`%s | ${site.siteMetadata.title}^блоґ`}
       meta={[
         {
           name: `description`,
